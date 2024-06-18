33ade8242608d170fd60df9fe5506a88fc1064db
alissa <alissasjuryadi@gmail.com>
2024-06-18 07:58:20 -0700

    Test clipboard API

--- index.js
+++ index.js
@@ -33,3 +33,9 @@ function generatePassword() {
 function copyPassword() {
     console.log(passwordLength);
 }
+
+navigator.permissions.query({ name: "write-on-clipboard" }).then((result) => {
+    if (result.state == "granted" || result.state == "prompt") {
+      alert("Write access granted!");
+    }
+  });
