602047abe627d776faefda78faf3eff22b182833
alissa <alissasjuryadi@gmail.com>
2024-06-18 09:58:08 -0700

    Add div for password options and password length customization option

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 "/"];
 
 const generateBtn = document.getElementById("generate-btn");
+const passwordSettingsBtn = document.getElementById("password-settings-btn");
+const passwordSettingsDiv = document.getElementById("password-settings-div");
 const optionOne = document.getElementById("option-one");
 const optionTwo = document.getElementById("option-two");
 const copyBtnOne = document.getElementById("copy-btn-one");
 const copyBtnTwo = document.getElementById("copy-btn-two");
 const passwordContainerOne = document.getElementById("password-container-one");
 const passwordContainerTwo = document.getElementById("password-container-two");
-
-let passwordLength = 15;
 
 function getRandomCharacter() {
     let randomChar = Math.floor(Math.random() * characters.length);
@@ -24,6 +24,7 @@ generateBtn.addEventListener("click", function() {
 });
 
 function generatePassword() {
+    const passwordLength = Number(document.getElementById("password-length").value);
     let generatedPassword = "";
     for (let i = 0; i < passwordLength; i++) {
         let char = getRandomCharacter();
@@ -46,3 +47,15 @@ function copyPassword(option) {
     navigator.clipboard.writeText(option.value);
     alert("Password has been copied to clipboard.");
 }
+
+function showPasswordOptions() {
+    if (passwordSettingsDiv.style.display === "none") {
+        passwordSettingsDiv.style.display = "block";
+    } else {
+        passwordSettingsDiv.style.display = "none";
+    }
+}
+
+passwordSettingsBtn.addEventListener("click", function() {
+    showPasswordOptions();
+});
